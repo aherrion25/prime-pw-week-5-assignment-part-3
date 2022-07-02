063be5a3b1a35ed470e848390106104e898207e9
Aubree Herrion <aubreeherrion@gmail.com>
2022-07-02 09:59:28 -0500

    started music collection file

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,22 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    console.log('The collection', collection);
+    return;
+}
+console.log(addToCollection());
+
+
+const album1 = {
+    title: 'Enter the Wu-Tang',
+    artist: 'Wu-Tang Clan',
+    yearPublished: 1993,
+}
+
+const album2 = {
+    title: 'Tical',
+    artist: 'Method Man',
+    yearPublished: 1994,
+}
