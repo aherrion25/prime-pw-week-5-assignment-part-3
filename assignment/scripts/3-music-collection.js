0fdb40e44c719400dc37cfbd655d0cdbc316cfb6
Aubree Herrion <aubreeherrion@gmail.com>
2022-07-02 19:38:11 -0500

    made changes to music collection file

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,22 +1,28 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+const collection = [];
 
 function addToCollection(title, artist, yearPublished) {
     console.log('The collection', collection);
-    return;
+    const newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished,
+    }
+    collection.push(newAlbum)
+    return newAlbum;
 }
-console.log(addToCollection());
+console.log(addToCollection('Tical', 'Method Man', 1994));
+console.log(addToCollection('36 Chambers', 'Wu-Tang Clan', 1993));
+console.log(addToCollection('Starting Over', 'Chris Stapleton', 2020));
+console.log(addToCollection('Permanent Vacation', 'Aerosmith', 1987));
+console.log(addToCollection('An Evening With Silk Sonic', 'Silk Sonic', 2021));
+console.log(addToCollection('LIVE.LOVE.A$AP', 'ASAP Rocky', 2011));
+console.log(collection);
+console.table(collection);
+
+function showCollection(params) {
+    
+}
 
 
-const album1 = {
-    title: 'Enter the Wu-Tang',
-    artist: 'Wu-Tang Clan',
-    yearPublished: 1993,
-}
-
-const album2 = {
-    title: 'Tical',
-    artist: 'Method Man',
-    yearPublished: 1994,
-}
